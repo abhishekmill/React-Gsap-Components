b5f9e94ed5ea725e578a74c905f4b4c2d2e2890f
abhisheklmill <speedlikeelectron@gmail.com>
2024-11-05 15:35:16 +0530

    created-track-btn

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import ParallaxContainer from "./comp/TrackButton";
+
+function App() {
+  return (
+    <>
+      <div className="w-full h-screen flex items-center justify-center">
+        <ParallaxContainer />
+        <ParallaxContainer />
+        <ParallaxContainer />
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/comp/TrackButton.jsx
@@ -0,0 +1,83 @@
+import React, { useRef, useEffect } from "react";
+import gsap from "gsap";
+
+const ParallaxContainer = ({
+  icon = "x",
+  hoverColor = "blue-600",
+  defaultColor = "black",
+}) => {
+  const containerRef = useRef(null);
+  const circleRef = useRef(null);
+  const iconRef = useRef(null);
+
+  useEffect(() => {
+    const container = containerRef.current;
+    const circle = circleRef.current;
+    const icon = iconRef.current;
+
+    const handleMouseLeave = () => {
+      gsap.to(container, { duration: 0.3 });
+      gsap.to([circle, icon], { duration: 0.3, scale: 1, x: 0, y: 0 });
+    };
+
+    const handleMouseEnter = () => {
+      gsap.to(container, { duration: 0.3 });
+      gsap.to(circle, { duration: 0.3, scale: 1 });
+    };
+
+    const handleMouseMove = (e) => {
+      callParallax(e);
+    };
+
+    const callParallax = (e) => {
+      parallaxIt(e, circle, 30);
+      parallaxIt(e, icon, 50);
+    };
+
+    const parallaxIt = (e, target, movement) => {
+      const relX = e.pageX - container.getBoundingClientRect().left;
+      const relY = e.pageY - container.getBoundingClientRect().top;
+      gsap.to(target, {
+        duration: 0.3,
+        x:
+          ((relX - container.offsetWidth / 2) / container.offsetWidth) *
+          movement,
+        y:
+          ((relY - container.offsetHeight / 2) / container.offsetHeight) *
+          movement,
+        ease: "power2.out",
+      });
+    };
+
+    container.addEventListener("mouseleave", handleMouseLeave);
+    container.addEventListener("mouseenter", handleMouseEnter);
+    container.addEventListener("mousemove", handleMouseMove);
+
+    return () => {
+      container.removeEventListener("mouseleave", handleMouseLeave);
+      container.removeEventListener("mouseenter", handleMouseEnter);
+      container.removeEventListener("mousemove", handleMouseMove);
+    };
+  }, []);
+
+  return (
+    <div
+      ref={containerRef}
+      className="group flex justify-center items-center relative w-[70px] h-[70px] overflow-hidden"
+    >
+      <div
+        ref={circleRef}
+        className={`circle absolute duration-150 h-[50px] w-[50px] border-2 m-10 rounded-full border-${defaultColor} group-hover:border-${hoverColor}`}
+      ></div>
+      <div
+        ref={iconRef}
+        className={`icon w-6 text-center group-hover:text-${hoverColor}`}
+        style={{ color: `var(--${defaultColor})` }}
+      >
+        {icon}
+      </div>
+    </div>
+  );
+};
+
+export default ParallaxContainer;
